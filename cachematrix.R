7e7930d54a26ace549770e0fe1e26620fa5b017d
cidrose <debarchanasarkar@yahoo.co.in>
2014-07-27 18:39:46 +0530

    Modified existing comments in header

--- cachematrix.R
+++ cachematrix.R
@@ -1,32 +1,31 @@
-
-makeCacheMatrix <- function( x = matrix() ) {
-m <- NULL
-set <- function( matrix ) {
-x <<- matrix
-m <<- NULL
+makeCacheMatrix <- function( x = matrix() ) {  ## Creates a special matrix object that the CacheSolve function can inverse
+m <- NULL                                      ## Initialize the inverse property
+set <- function( matrix ) {                    ## Sets the matrix
+x <<- matrix                                      
+m <<- NULL                                     
 }
-get <- function() {
-x
+get <- function() {                            ## Gets the matrix 
+x                                              ## Returns matrix
 }
-setinverse <- function(inverse) {
+setinverse <- function(inverse) {              ## Sets inverse of the matrix
 m <<- inverse
 }
-getinverse <- function() {
-m
+getinverse <- function() {                     ## Gets inverse of the matrix              
+m                                              ## Returns inverse 
 }
-list(set = set, get = get,
+list(set = set, get = get,                     ## Returns a method list 
 setinverse = setinverse,
 getinverse = getinverse)
 }
 
-cacheSolve <- function(x, ...) {
-m <- x$getinverse()
-if( !is.null(m) ) {
+cacheSolve <- function(x, ...) {               ## CacheSolve returns inverse of matrix created with makeCacheMatrix 
+m <- x$getinverse()                            ## Returns a matrix inverse of 'x'
+if( !is.null(m) ) {                            ## Returns inverse if its already set 
 message("getting cached data")
 return(m)
 }
-data <- x$get()
-m <- solve(data) %*% data 
-x$setinverse(m)
-m
+data <- x$get()                                ## Gets matrix from object
+m <- solve(data) %*% data                      ## Matrix multiplication calculates inverse
+x$setinverse(m)                                ## Sets inverse to object
+m                                              ## Returns matrix
 }
